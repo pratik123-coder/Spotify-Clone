c0c605f392969017f00d1e22339584df584adbd3
pratik-win-PC <pratiktravelogue@gmail.com>
2023-12-14 11:15:15 +0530

    Account Page Added. User Not able to fetch(SUpabase Schema Error)

---
+++ app/account/components/AccountContent.tsx
@@ -0,0 +1,51 @@
+"use client"
+import { useUser } from "@/hooks/useUser";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+import { useSessionContext } from "@supabase/auth-helpers-react";
+
+const AccountContent = () => {
+  const router = useRouter();
+  const { supabaseClient } = useSessionContext();
+  const { isLoading, user } = useUser();
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data, error } = await supabaseClient
+          .from('users')
+          .select('id', 'full_name') // Include other fields as needed
+          .eq('id', user?.id)
+          .single();
+
+        if (error) {
+          throw error;
+        }
+
+        console.log("User Data:", data); // Log the data to the console
+
+        setUserData(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error.message);
+      }
+    };
+
+    fetchData(); // Call fetchData function
+
+  }, [isLoading, user, router, supabaseClient]);
+
+  return (
+    <div>
+      {userData ? (
+        <>
+          <h1 className="ps-6 text-bold text-2xl">Welcome, {userData.full_name}!</h1>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default AccountContent;
---
+++ app/account/page.tsx
@@ -0,0 +1,25 @@
+import Header from "@/components/Header";
+import AccountContent from "./components/AccountContent";
+
+const Account = () => {
+  return ( 
+    <div className="
+    bg-neutral-900
+    rounded-lg
+    w-full
+    h-screen
+    overflow-hidden
+    overflow-y-auto
+    "><Header className="from-bg-neutral-900">
+      <div className="md-2 flex flex-col gap-y-6">
+        <h1 className="text-white text-3xl font-semibold">
+          Account Information
+        </h1>
+      </div>
+    </Header>
+    <AccountContent/>
+    </div>
+  );
+}
+ 
+export default Account;
